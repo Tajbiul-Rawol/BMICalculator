aa4561c4013e9c10f621df1c5a6f1cbf6e5dc630
tajbiul rawol <tajbiulrawol@gmail.com>
2021-07-06 23:57:20 +0600

    validation for fields

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ button.addEventListener('click', ()=>{
 
     let cm = document.getElementById("cm").value;
     let kg = document.getElementById("kg").value;
-
+    
+    if (cm.length == 0) {
+        document.getElementById("cmerror").innerHTML = "Height cannot be empty!";
+        return 0;
+    }
+    if (kg.length == 0) {
+        document.getElementById("kgerror").innerHTML = "Weight cannot be empty!";
+        return 0;
+    }
+    document.getElementById("cmerror").innerHTML = " ";
+    document.getElementById("kgerror").innerHTML = " ";
+    
     let bmi = ((kg / (cm*cm))*10000).toFixed(2); 
-
     document.getElementById("result").innerHTML = bmi;
+
+    
 });
